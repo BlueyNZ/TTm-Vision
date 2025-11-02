d7a04773304473e827a609d6edc5a15a1e091925
Harrison Price <harrisonprice124@gmail.com>
2025-11-02 10:58:44 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,4 @@
-// src/firebase/config.ts
-import { FirebaseOptions } from 'firebase/app';
-
-const firebaseConfig: FirebaseOptions = {
+export const firebaseConfig = {
   "projectId": "studio-4955279472-f5d23",
   "appId": "1:968278479885:web:ce97a9d5bbb4bc0c0cf144",
   "apiKey": "AIzaSyAeFSmEVEINcY2IN_AKB9pKazaQuYGVuUc",
@@ -9,10 +6,3 @@ const firebaseConfig: FirebaseOptions = {
   "measurementId": "",
   "messagingSenderId": "968278479885"
 };
-
-export function getFirebaseConfig() {
-  if (!firebaseConfig.apiKey) {
-    throw new Error('Missing Firebase configuration');
-  }
-  return firebaseConfig;
-}
