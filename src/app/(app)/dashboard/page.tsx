b05669ee2e6c92bb0ce0a2d271c658a0e21ba994
Harrison Price <harrisonprice124@gmail.com>
2025-11-04 03:45:11 +0000

    can we remove the notification thing?

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -102,16 +102,7 @@ export default function DashboardPage() {
             </CardDescription>
           </CardHeader>
            <CardContent>
-            <Button
-              onClick={() => {
-                toast({
-                  title: "Scheduled: Catch up",
-                  description: "Friday, February 10, 2023 at 5:57 PM",
-                });
-              }}
-            >
-              Show Notification
-            </Button>
+           
           </CardContent>
         </Card>
 
