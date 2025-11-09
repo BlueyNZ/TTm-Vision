1389871ac20ae8b591c312bf8fefe71b43e8fc45
Harrison Price <harrisonprice124@gmail.com>
2025-11-09 02:03:15 +0000

    make it so that it track the jobs already in there like if there is a jo

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -281,7 +281,7 @@ export function AppSidebar({ isAdmin }: AppSidebarProps) {
           </Card>
         </div>
         <div className="flex justify-center p-2 group-data-[collapsible=icon]:hidden">
-          <Badge variant="secondary">v0.2 Beta</Badge>
+          <Badge variant="secondary">v0.2</Badge>
         </div>
         <SidebarMenu>
           <SidebarMenuItem>
@@ -324,3 +324,5 @@ export function AppSidebar({ isAdmin }: AppSidebarProps) {
     </Sidebar>
   );
 }
+
+    
