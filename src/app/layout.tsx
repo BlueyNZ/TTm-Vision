05eecca53c337672b0c313629a30ced8bf7842ba
Harrison Price <harrisonprice124@gmail.com>
2025-11-18 07:19:52 +0000

    okay so when i bookmark the website on my iphone the picture for the boo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { FirebaseClientProvider } from '@/firebase/client-provider';
 export const metadata: Metadata = {
   title: 'TTM Vision',
   description: 'Simplified Traffic Management',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -23,6 +24,9 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>{children}</FirebaseClientProvider>
